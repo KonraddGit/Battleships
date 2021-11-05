d69142cc2067373bf30e1382b37e5dc8d7c3ac9a
KonraddGit <61412913+KonraddGit@users.noreply.github.com>
2021-11-05 11:20:26 +0100

    Placing Every Ship, need fix

--- Battleships.Logic/BoardLogic/GenerateShips.cs
+++ Battleships.Logic/BoardLogic/GenerateShips.cs
@@ -10,6 +10,7 @@ namespace Battleships.Logic.BoardLogic
 {
     public class GenerateShips : IShipGenerator
     {
+        List<Cell> PlacedShips = new List<Cell>();
         public ICell Cell { get; set; } = new CellLogic();
         private static readonly Random rnd = new();
 
@@ -19,20 +20,20 @@ namespace Battleships.Logic.BoardLogic
                 PlaceShipOnBoard(ship);
         }
 
-        private Cell FindNextSpot(List<Cell> cell = null)
+        private Cell FindNextSpot()
         {
             var newPosition = Cell.FirstPosition();
 
-            if (cell == null)
+            if (PlacedShips == null)
                 return newPosition;
 
-            foreach (var position in cell)
+            foreach (var position in PlacedShips)
                 if (position == newPosition)
-                    FindNextSpot(cell);
+                    FindNextSpot();
 
-            foreach (var position in cell)
+            foreach (var position in PlacedShips)
                 if (!SpaceBetweenShips(position, newPosition))
-                    FindNextSpot(cell);
+                    FindNextSpot();
 
             return newPosition;
         }
@@ -53,35 +54,76 @@ namespace Battleships.Logic.BoardLogic
                 return true;
         }
 
+        private void BuildShipWithGivenLength(int length)
+        {
+            var cell = PlacedShips.Last();
+            Cell tmp = null;
+
+            var direction = rnd.Next(0, 3);
+            switch (direction)
+            {
+                case 0:
+                    tmp = new Cell { X = cell.X - 1, Y = cell.Y };
+                    break;
+                case 1:
+                    tmp = new Cell { X = cell.X, Y = cell.Y + 1 };
+                    break;
+                case 2:
+                    tmp = new Cell { X = cell.X + 1, Y = cell.Y };
+                    break;
+                case 3:
+                    tmp = new Cell { X = cell.X, Y = cell.Y - 1 };
+                    break;
+                default:
+                    break;
+            }
+
+            for (int i = 1; i < length; i++)
+                PlacedShips.Add(Cell.PointTypeDraw(DrawType.ShipMark, tmp));
+        }
+
         private void PlaceShipOnBoard(IShip ship)
         {
-            var placedShips = new List<Cell>();
-
             for (int i = 0; i < ship.ShipNumber; i++)
                 switch (ship.Size)
                 {
                     case 1:
-                        placedShips.Add(Cell.PointTypeDraw(DrawType.ShipMark, FindNextSpot(placedShips)));
+                        PlacedShips.Add(Cell.PointTypeDraw(DrawType.ShipMark, FindNextSpot()));
                         break;
 
                     case 2:
-                        //Cell.PointTypeDraw(DrawType.ShipMark, FindNextSpot(first));
-                        //var direction = rnd.Next(1, 4);
-
+                        PlacedShips.Add(Cell.PointTypeDraw(DrawType.ShipMark, FindNextSpot()));
+                        BuildShipWithGivenLength(ship.Size);
                         break;
 
                     case 3:
+                        PlacedShips.Add(Cell.PointTypeDraw(DrawType.ShipMark, FindNextSpot()));
+                        BuildShipWithGivenLength(ship.Size);
                         break;
 
                     case 4:
+                        PlacedShips.Add(Cell.PointTypeDraw(DrawType.ShipMark, FindNextSpot()));
+                        BuildShipWithGivenLength(ship.Size);
                         break;
 
                     case 5:
+                        PlacedShips.Add(Cell.PointTypeDraw(DrawType.ShipMark, FindNextSpot()));
+                        BuildShipWithGivenLength(ship.Size);
                         break;
 
                     default:
                         break;
                 }
+
+            DisplayShipPositions(PlacedShips);
+        }
+
+        private static void DisplayShipPositions(List<Cell> placedShips)
+        {
+            foreach (var item in placedShips)
+            {
+                Console.WriteLine($"[{item.X}, {item.Y}]");
+            }
         }
     }
 }
